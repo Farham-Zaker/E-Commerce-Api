ff19fda23dd9379ef1e1a2e2a14e69cfbfa1367c
Farham <farham.zaker007@gmail.com>
2023-09-04 00:28:41 +0430

    Edit dotenv config

--- src/middlewares/projectConfigs.ts
+++ src/middlewares/projectConfigs.ts
@@ -1,9 +1,8 @@
 import express, { Express } from "express";
-
-import * as dotenv from "dotenv"
-dotenv.config()
+import * as dotenv from "dotenv";
 
 export default (server: Express) => {
   server.use(express.json());
   server.use(express.urlencoded({ extended: true }));
+  dotenv.config();
 };
