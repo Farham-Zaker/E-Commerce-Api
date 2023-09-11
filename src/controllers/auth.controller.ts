5e886e409a8641d16a597a900bd9f6a018185ed3
Farham <farham.zaker007@gmail.com>
2023-09-11 22:12:26 +0430

    Create registration controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,3 +1,5 @@
 import { Request, Response } from "express";
 
-export default new (class Controller {})();
+export default new (class Controller {
+  async registerRoute(req: Request, res: Response): Promise<void> {}
+})();
