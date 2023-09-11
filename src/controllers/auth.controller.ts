74261e550ee6445c79bdb3adcd37f0391a1db968
Farham <farham.zaker007@gmail.com>
2023-09-12 02:10:22 +0430

    Handle exceptional errors during registration

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -46,6 +46,8 @@ export default new (class Controller {
         });
       } else {
       }
-    } catch (error) {}
+    } catch (error) {
+      throw new Error("There is an error in registeration." + error)
+    }
   }
 })();
