0a5b4b69d4b1af419ac2165d4263e6a83af8f6b8
Farham <farham.zaker007@gmail.com>
2023-09-12 02:30:04 +0430

    Add interface for new user data for register route

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -5,3 +5,11 @@ export interface RegistrationUserInputInt {
   phone: string;
   password: string;
 }
+export interface RegistrationUserDataInt {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  image: string;
+}
