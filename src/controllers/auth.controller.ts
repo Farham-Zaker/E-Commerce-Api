35e64f1261021c4e48daebee3f1141fb4f82d293
Farham <farham.zaker007@gmail.com>
2023-09-11 22:23:39 +0430

    Add user input

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
+import prismaService from "./../prisma/prismaService";
+
+import hashPassword from "./../middlewares/hashPassword";
 
 export default new (class Controller {
-  async registerRoute(req: Request, res: Response): Promise<void> {}
+  async registerRoute(req: Request, res: Response): Promise<void> {
+
+    const { firstName, lastName, email, phone, password } = req.body;
+
+  }
 })();
