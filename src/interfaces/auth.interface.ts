7ddfa65f242223dffbae8139d04debcea4384fc3
Farham <farham.zaker007@gmail.com>
2023-09-14 00:59:41 +0430

    feat(register controller): Add interfaces

---
+++ src/interfaces/auth.interface.ts
@@ -0,0 +1,27 @@
+export interface RegistrationBodyRequestTypes {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  password: string;
+}
+export interface UserDataTypes {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  image: string;
+  createdAt: Date;
+}
+export interface RegistrationSuccessResponseTypes {
+  message: string;
+  statusCode: number;
+  response: string;
+  data: UserDataTypes;
+}
+export interface RegistrationّFailedResponseTypes {
+  message: string;
+  statusCode: number;
+  response: string;
+}
