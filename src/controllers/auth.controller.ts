661504aea19c0333bf31a4573cc13116bb8d053a
Farham <farham.zaker007@gmail.com>
2023-09-14 01:49:29 +0430

    feat(controller): Add registration route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,3 +1,70 @@
 import { Request, Response } from "express";
+import prismaService from "./../prisma/prismaService";
+import hashPassword from "./../middlewares/hashPassword";
+import {
+  RegistrationBodyRequestTypes,
+  RegistrationSuccessResponseTypes,
+  RegistrationّFailedResponseTypes,
+  UserDataTypes,
+} from "./../interfaces/auth.interface";
 
-export default new (class Controller {})();
+export default new (class Controller {
+  async registerRoute(req: Request, res: Response): Promise<void> {
+    const {
+      firstName,
+      lastName,
+      email,
+      phone,
+      password,
+    }: RegistrationBodyRequestTypes = req.body;
+
+    try {
+      const user = await prismaService.users.findFirst({
+        where: {
+          OR: [
+            {
+              email,
+            },
+            { phone },
+          ],
+        },
+      });
+
+      if (!user) {
+        const hashedPassword: string = await hashPassword(password);
+
+        const newUser: UserDataTypes = await prismaService.users.create({
+          data: {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            image: "",
+            phone: phone,
+            auth: {
+              create: {
+                password: hashedPassword,
+                token: "",
+              },
+            },
+          },
+        });
+        const response: RegistrationSuccessResponseTypes = {
+          message: "Created",
+          statusCode: 201,
+          response: "The account has been successfully created.",
+          data: newUser,
+        };
+        res.status(201).json(response);
+      } else {
+        const response: RegistrationّFailedResponseTypes = {
+          message: "Conflict",
+          statusCode: 409,
+          response: "An account with similar information already exists.",
+        };
+        res.status(409).json(response);
+      }
+    } catch (error) {
+      throw new Error("There is an error in registeration process." + error);
+    }
+  }
+})();
