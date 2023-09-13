0981f19d61e92c2fbf3d91a0eec410736f9fc6a9
Farham <farham.zaker007@gmail.com>
2023-09-13 10:19:55 +0430

    Specify data types for 'req.body'

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,10 +2,11 @@ import { Request, Response } from "express";
 import prismaService from "./../prisma/prismaService";
 
 import hashPassword from "./../middlewares/hashPassword";
+import { RegistrationUserInputInt } from "./../interfaces/auth.interface";
 
 export default new (class Controller {
   async registerRoute(req: Request, res: Response): Promise<void> {
-    const { firstName, lastName, email, phone, password } = req.body;
+    const { firstName, lastName, email, phone, password }:RegistrationUserInputInt = req.body;
 
     try {
       const user = await prismaService.users.findFirst({
