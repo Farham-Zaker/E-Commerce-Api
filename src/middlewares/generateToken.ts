e748774e76d12787e63b2c85fd89aed0fb366fc1
Farham <farham.zaker007@gmail.com>
2023-09-15 02:33:34 +0430

    chore(login): Edit constant name in generate token middleware

--- src/middlewares/generateToken.ts
+++ src/middlewares/generateToken.ts
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export default function generateToken(userId: string): string  {
-  const jwt_secret_key: string | undefined = process.env.JWT_SECRET_KEY;
+  const JWT_SECRET_KEY: string | undefined = process.env.JWT_SECRET_KEY;
 
-  if (typeof jwt_secret_key === "string") {
-    const token: string = jwt.sign({ userId }, jwt_secret_key, {
+  if (typeof JWT_SECRET_KEY === "string") {
+    const token: string = jwt.sign({ userId }, JWT_SECRET_KEY, {
       expiresIn: Date.now() + 100 * 60 * 60 * 24 * 30 * 3,
     });
     return token;
