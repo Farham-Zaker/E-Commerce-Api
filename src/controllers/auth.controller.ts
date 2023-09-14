5d4ddb0126b51ace76cd93252fc7766b3a9df49a
Farham <farham.zaker007@gmail.com>
2023-09-15 03:26:39 +0430

    feat(login): Add login controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,7 +6,15 @@ import {
   RegistrationSuccessResponseTypes,
   RegistrationّFailedResponseTypes,
   UserDataTypes,
+  LoginUserInputTypes,
+  LoginUserDataTypes,
+  LoginSuccessfulMessageTypes,
+  LoginFailedMessageMessageTypes,
 } from "./../interfaces/auth.interface";
+
+import userFinder from "../middlewares/userFinder";
+import passwordValidator from "../middlewares/passwordValidator";
+import generateToken from "../middlewares/generateToken";
 
 export default new (class Controller {
   async registerRoute(req: Request, res: Response): Promise<void> {
@@ -65,6 +73,45 @@ export default new (class Controller {
       }
     } catch (error) {
       throw new Error("There is an error in registeration process." + error);
+    }
+  }
+  async loginRoute(req: Request, res: Response): Promise<void> {
+    const { phoneOrEmail, password }: LoginUserInputTypes = req.body;
+
+    try {
+      const user: LoginUserDataTypes | null = await userFinder(phoneOrEmail);
+
+      if (user && user?.auth) {
+        const isPasswordValid: boolean | undefined = await passwordValidator(
+          password,
+          user.auth?.password
+        );
+        if (isPasswordValid) {
+          const token: string = generateToken(user.auth?.password);
+          const successfulResponse: LoginSuccessfulMessageTypes = {
+            message: "Login successfully",
+            statusCode: 200,
+            token,
+          };
+          res.status(200).json(successfulResponse);
+        } else {
+          const failedResponse: LoginFailedMessageMessageTypes = {
+            message: "Incorrect password",
+            statusCode: 401,
+            response: "The password you sent is incorrect.",
+          };
+          res.status(401).json(failedResponse);
+        }
+      } else {
+        const failedResponse: LoginFailedMessageMessageTypes = {
+          message: "Not Found",
+          statusCode: 404,
+          response: "User with given profile not found",
+        };
+        res.status(404).json(failedResponse);
+      }
+    } catch (error) {
+      throw new Error("There is an error in logining process." + error);
     }
   }
 })();
