57ff7a5c7aa0703f6a2e87961f745b8eb5d7fdf7
Farham <farham.zaker007@gmail.com>
2023-09-15 01:13:13 +0430

    feat(login): Add userFinder middleware for searching users by email or phone

---
+++ src/middlewares/userFinder.ts
@@ -0,0 +1,20 @@
+import prismaService from "./../prisma/prismaService";
+import validator from "validator";
+
+async function userFinder(phoneOrEmail: string) {
+  if (validator.isEmail(phoneOrEmail)) {
+    const user = await prismaService.users.findFirst({
+      where: { email: phoneOrEmail },
+      include: {auth:true}
+    });
+    return user;
+  } else {
+    const user = await prismaService.users.findFirst({
+      where: { phone: phoneOrEmail },
+      include: {auth:true}
+    });
+    return user;
+  }
+}
+
+export default userFinder;
