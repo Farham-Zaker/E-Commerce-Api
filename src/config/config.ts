7d2c3a71793de6852b5d44b188e92508f54711ff
Farham <farham.zaker007@gmail.com>
2023-09-17 02:54:51 +0430

    feat(config): Add callback url for google auth to config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,10 @@
 const config: configInt = {
   port: 7000,
+  googleAuthCallbackUrl: "http://localhost:7000/auth/google/callback",
 };
 interface configInt {
   port: number;
+  googleAuthCallbackUrl: string;
 }
 
 export default config;
