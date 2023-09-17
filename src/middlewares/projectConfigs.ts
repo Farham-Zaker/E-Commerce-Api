000ae3dfc9cd2455ce627171089c22ce2b068b23
Farham <farham.zaker007@gmail.com>
2023-09-17 15:11:37 +0430

    feat(config): Add passport,express-session and oAuth strategy to projectConfig.ts

--- src/middlewares/projectConfigs.ts
+++ src/middlewares/projectConfigs.ts
@@ -1,8 +1,22 @@
 import express, { Express } from "express";
 import * as dotenv from "dotenv";
+import passport from "passport";
+import session from "express-session";
+import oAuthSetup from "./../passport/oAuthGoogle";
 
-export default (server: Express) => {
+export default async (server: Express) => {
   server.use(express.json());
   server.use(express.urlencoded({ extended: true }));
   dotenv.config();
+  server.use(
+    session({
+      secret: process.env.SESSION_SECRET as string,
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
+
+  server.use(passport.initialize());
+  server.use(passport.session());
+  oAuthSetup();
 };
