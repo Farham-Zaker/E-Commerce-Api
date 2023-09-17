61b72f5dc7f1ac3a3c816b87483137e5c48a3677
Farham <farham.zaker007@gmail.com>
2023-09-17 17:43:27 +0430

    feat(google auth): Add passport middlewares to '/google','/google/callback' routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 const router = Router()
 import authController from '../controllers/auth.controller';
+import passport = require('passport');
 
 router.post('/register', authController.registerRoute)
 router.post('/login', authController.loginRoute)
 
-router.get('/google', )
-router.get('/google/callback', )
+router.get('/google', passport.authenticate("google",{scope:["email","profile"]}))
+router.get('/google/callback', passport.authenticate("google"),authController.googleCallbackRoute)
 export default router;
