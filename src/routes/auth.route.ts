765151ef6ed1b101fd1b972807b6670b9983d136
Farham <farham.zaker007@gmail.com>
2023-09-19 14:52:50 +0430

    feat(auth): Update auth.route.ts

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,18 @@
-import { Router } from 'express';
-const router = Router()
-import authController from '../controllers/auth.controller';
-import passport = require('passport');
+import { Router } from "express";
+const router = Router();
+import authController from "../controllers/auth.controller";
+import passport = require("passport");
 
-router.post('/register', authController.registerRoute)
-router.post('/login', authController.loginRoute)
+router.post("/register", authController.registerRoute);
+router.post("/login", authController.loginRoute);
 
-router.get('/google', passport.authenticate("google",{scope:["email","profile"]}))
-router.get('/google/callback', passport.authenticate("google"),authController.googleCallbackRoute)
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google"),
+  authController.googleCallbackRoute
+);
 export default router;
