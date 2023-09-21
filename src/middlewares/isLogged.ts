7f32615913bd64f62962becd95a6c6ebf8ac5be9
Farham <farham.zaker007@gmail.com>
2023-09-21 09:11:05 +0430

    feat(auth): Add isLogged middleware to check token validation

---
+++ src/middlewares/isLogged.ts
@@ -0,0 +1,37 @@
+import jwt, { verify, JwtPayload, Secret } from "jsonwebtoken";
+import { RequestHandler, Request, Response, NextFunction } from "express";
+import decodeToken from "./decodeToekn";
+
+interface ErrorResponseTypes {
+  message: string;
+  statusCode: number;
+  response: string;
+}
+
+const isLogged: RequestHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token: string = req.header("token")!;
+
+  const errorResponse: ErrorResponseTypes = {
+    message: "bad",
+    statusCode: 401,
+    response: "Your token is expired or invalid.Login again.",
+  };
+
+  if (token) {
+    const decodedToken = decodeToken(token);
+
+    if (decodedToken) {
+      next();
+    } else {
+      res.status(401).json(errorResponse);
+    }
+  } else {
+    res.status(401).json(errorResponse);
+  }
+};
+
+export default isLogged;
