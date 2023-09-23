e0faa9c9507a03c59718b265fb818ff64ca3025e
Farham <farham.zaker007@gmail.com>
2023-09-23 18:10:11 +0330

    feat(sub routes): Add '/account' route

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,4 +4,7 @@ const router = Router();
 import authRoute from "../routes/auth.route";
 router.use("/auth", authRoute);
 
+import accountRoute from "./../routes/account.route"
+router.use('/account', accountRoute)
+
 export default router;
