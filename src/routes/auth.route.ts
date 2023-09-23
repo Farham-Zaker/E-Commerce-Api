df0d8313be3953d88223fb67f7f3cfa1601ccf0d
Farham <farham.zaker007@gmail.com>
2023-09-23 18:04:06 +0330

    feat(auth): Use validators

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
 const router = Router();
 import authController from "../controllers/auth.controller";
+import authValidator from "../validators/auth.validator";
+import validationResults from "../validators/validationResults";
 import passport = require("passport");
 
-router.post("/register", authController.registerRoute);
-router.post("/login", authController.loginRoute);
+router.post(
+  "/register",
+  authValidator.registerValidator(),
+  validationResults,
+  authController.registerRoute
+);
+router.post(
+  "/login",
+  authValidator.loginValidator(),
+  validationResults,
+  authController.loginRoute
+);
 
 router.get(
   "/google",
