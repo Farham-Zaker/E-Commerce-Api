8bef4486c6cc065a5909c75274ea1ba18e78908c
Farham <farham.zaker007@gmail.com>
2023-09-26 00:50:52 +0330

    feat(get product by fillter): Add '/getByFilter' route

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -4,5 +4,6 @@ import productController from "../controllers/product.controller";
 
 router.get("/", productController.getAllProduct);
 router.get('/:productId', productController.getProductByIdRoute)
+router.get('/getByFilter', )
 
 export default router;
