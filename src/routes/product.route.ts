e9bc9e4c71e5f389d0a36517ebfa7f6eab049690
Farham <farham.zaker007@gmail.com>
2023-09-25 21:35:28 +0330

    feat(get product by id): Add route

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -3,5 +3,6 @@ const router = Router();
 import productController from "../controllers/product.controller";
 
 router.get("/", productController.getAllProduct);
+router.get('/:productId', )
 
 export default router;
