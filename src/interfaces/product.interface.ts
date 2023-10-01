492a810505fef89aa1051d94e732cee4853076e6
Farham <farham.zaker007@gmail.com>
2023-10-02 01:46:34 +0330

    refactor(products): Update product types

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -2,31 +2,25 @@ export interface ProductTypes {
   productId: string;
   title: string;
   price: number;
-  image: string;
+  image: string | null;
   discountStatus?: number | null;
   discountPercent?: number | null;
-  discountEndTime?: Date | null;
-  priceAfterDiscount?: number | null;
-  createdAt: Date;
-  categoryId: string;
-  category: Category;
-  inventories: InventoryTypes[];
-}
-interface Category {
-  categoryId: string;
-  name: string;
-}
-
-interface InventoryTypes {
-  inventoryId: string;
-  quantity: number;
-  productId: string;
-  colorId: string;
-  colors: {
-    colorId: string;
+  dicountEndTime?: Date | null;
+  discountedPrice?: number | null;
+  createdAt?: Date;
+  category?: {
+    categoryId: string;
     name: string;
-    hexCode: string;
   };
+  inventories?:
+    | {
+        quantity: number;
+        colors: {
+          colorId?: string | null;
+          name: string;
+          hexCode: string;
+        };
+      }[];
 }
 export interface FilterProductRquestBodyTypes {
   colorIds?: string[];
