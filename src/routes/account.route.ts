42570d7f6834d4b88d4908589361123a481001a4
Farham <farham.zaker007@gmail.com>
2023-10-05 11:52:50 +0330

    feat(account): Add isLogged middleware for all route

--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -2,28 +2,33 @@ import { Router } from "express";
 import accountController from "../controllers/account.controller";
 import accountValidator from "../validators/account.validator";
 import validationResults from "../validators/validationResults";
+import isLogged from "../middlewares/isLogged";
 const router = Router();
 
 router.put(
   "/update-info",
+  isLogged,
   accountValidator.updateInfoValidator(),
   validationResults,
   accountController.updateInfoRoute
 );
 router.put(
   "/set-password",
+  isLogged,
   accountValidator.setPasswordValidator(),
   validationResults,
   accountController.setPasswordRoute
 );
 router.put(
   "/change-password",
+  isLogged,
   accountValidator.changePasswordValidator(),
   validationResults,
   accountController.changePasswordRoute
 );
 router.get(
   "/info",
+  isLogged,
   accountValidator.getUserInfoValidator(),
   validationResults,
   accountController.getUserInfo
