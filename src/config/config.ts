0b9aacd7c1abdbda9f65b27e9378a153b914151e
Farham <farham.zaker007@gmail.com>
2023-10-13 02:21:47 +0330

    feat(config): Add merchant_id & callback url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,14 @@
 const config: configInt = {
   port: 7000,
   googleAuthCallbackUrl: "http://localhost:7000/auth/google/callback",
+  merchant_id: "6cded376-3063-11e9-a98e-005056a205be",
+  gateway_callback: "http://localhost:7000/payment/paycallback",
 };
 interface configInt {
   port: number;
   googleAuthCallbackUrl: string;
+  merchant_id: string;
+  gateway_callback: string;
 }
 
 export default config;
