e371fa12c5143681e6308fdef7def769714f201e
Farham <farham.zaker007@gmail.com>
2023-10-13 02:28:35 +0330

    feat(pay callback): Add '/payCallback"' route

--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -4,5 +4,6 @@ import paymentController from "../controllers/payment.controller";
 const router = Router();
 
 router.post("/pay", isLogged, paymentController.pay);
+router.get("/payCallback");
 
 export default router;
