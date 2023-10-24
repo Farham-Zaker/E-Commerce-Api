06dbaf1987975ea94418d85a260d667f8f61e284
Farham-Zaker <farham.zaker007@gmail.com>
2023-10-25 00:00:14 +0330

    refactor(account): Remove isLogged middleware from all routes

--- src/routes/account.route.ts
+++ src/routes/account.route.ts
@@ -2,34 +2,29 @@ import { Router } from "express";
 import accountController from "../controllers/account.controller";
 import accountValidator from "../validators/account.validator";
 import validationResults from "../validators/validationResults";
-import isLogged from "../middlewares/isLogged";
 const router = Router();
 
 router.put(
   "/update-info",
-  isLogged,
   accountValidator.updateInfoValidator(),
   validationResults,
   accountController.updateInfoRoute
 );
-router.post("/upload-image", isLogged, accountController.uploadImage);
+router.post("/upload-image", accountController.uploadImage);
 router.put(
   "/set-password",
-  isLogged,
   accountValidator.setPasswordValidator(),
   validationResults,
   accountController.setPasswordRoute
 );
 router.put(
   "/change-password",
-  isLogged,
   accountValidator.changePasswordValidator(),
   validationResults,
   accountController.changePasswordRoute
 );
 router.get(
   "/info",
-  isLogged,
   accountValidator.getUserInfoValidator(),
   validationResults,
   accountController.getUserInfo
