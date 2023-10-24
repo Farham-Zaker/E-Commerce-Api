d657aadd1f49235d30485d14169d6ef4dd548a0d
Farham-Zaker <farham.zaker007@gmail.com>
2023-10-25 00:09:35 +0330

    refactor(payment): Remove isLogged middleware from all routes

--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import isLogged from "../middlewares/isLogged";
 import paymentController from "../controllers/payment.controller";
 const router = Router();
 
-router.post("/pay", isLogged, paymentController.pay);
+router.post("/pay", paymentController.pay);
 router.get("/payCallback", paymentController.payCallback);
-router.get("/get", isLogged, paymentController.getAllPeyments);
+router.get("/get", paymentController.getAllPeyments);
 
 export default router;
