55ae27c53a5be797b02d10eab99d586fce837338
Farham-Zaker <farham.zaker007@gmail.com>
2023-10-24 21:08:15 +0330

    feat(admin): Add interface for '/get' controller

---
+++ src/admin/interfaces/order.inteface.ts
@@ -0,0 +1,46 @@
+export interface GetOrdersRouteOrdersTypes {
+  orderId: string;
+  userId: string;
+  totalPrice: number;
+  status: string;
+  createdAt: Date;
+  orderItems?: OrderIthem[];
+  user?: UserTypes;
+  payment?: PaymentTypes;
+}
+interface OrderIthem {
+  orderItemId: string;
+  orderId: string;
+  productId: string;
+  quantity: number;
+  colorId: string;
+  product: ProductTypes;
+}
+interface ProductTypes {
+  productId: string;
+  title: string;
+  price: number;
+  discountStatus: number;
+  discountPercent: number;
+  discountEndTime: Date;
+  finalPrice: number;
+  createdAt: string;
+  categoryId: string;
+}
+interface UserTypes {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  image: string;
+  createdAt: Date;
+}
+interface PaymentTypes {
+  paymentId: string;
+  authorityId: string;
+  amount: number;
+  status: string;
+  orderId: string;
+  createdAt: string;
+}
