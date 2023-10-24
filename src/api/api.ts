be7f089d689fef681d56d7ec16253bb34a3b3e6a
Farham-Zaker <farham.zaker007@gmail.com>
2023-10-25 00:08:50 +0330

    feat(comment): Add isLogged middleware for sub route

--- src/api/api.ts
+++ src/api/api.ts
@@ -21,7 +21,7 @@ import likesRoute from "../routes/likes.route";
 router.use("/likes", isLogged, likesRoute);
 
 import commentRoute from "../routes/comments.route";
-router.use("/comments", commentRoute);
+router.use("/comments", isLogged, commentRoute);
 
 import paymentRoute from "../routes/payment.route";
 router.use("/payment", paymentRoute);
