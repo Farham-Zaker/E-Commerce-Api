c5f6a30b49d8ab6ba8afab3d57827dd0bf9819db
Farham-Zaker <farham.zaker007@gmail.com>
2023-10-25 00:05:02 +0330

    refactore(cart): Remove isLogged middleware from all routes

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import isLogged from "../middlewares/isLogged";
 import cartValidator from "../validators/cart.validator";
 import validationResults from "../validators/validationResults";
 import cartController from "../controllers/cart.controller";
@@ -7,20 +6,18 @@ const router = Router();
 
 router.post(
   "/add",
-  isLogged,
   cartValidator.addToCartValidator(),
   validationResults,
   cartController.addToCarts
 );
-router.get("/get", isLogged, cartController.getAllCarts);
-router.get("/get/:cartId", isLogged, cartController.getCartById);
+router.get("/get", cartController.getAllCarts);
+router.get("/get/:cartId", cartController.getCartById);
 router.put(
   "/update",
-  isLogged,
   cartValidator.updateCartValidator(),
   validationResults,
   cartController.updateCarts
 );
-router.delete("/delete/:cartId", isLogged, cartController.deleteCartByIdreq);
+router.delete("/delete/:cartId", cartController.deleteCartByIdreq);
 
 export default router;
