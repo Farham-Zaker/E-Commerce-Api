321ca4fb355dbbf9d5334538713d290c7a28f7d4
Farham-Zaker <farham.zaker007@gmail.com>
2023-10-29 18:02:17 +0330

    feat(admin-user): Add validator for '/create' route

--- src/admin/routes/user.route.ts
+++ src/admin/routes/user.route.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import userValidator from "../validators/user.validator";
+import validationResults from "../../validators/validationResults";
 const router = Router();
 
 router.post(
   "/create",
+  userValidator.createOrder(),
+  validationResults,
 );
 
 
---
+++ src/admin/validators/user.validator.ts
@@ -0,0 +1,48 @@
+import { check, ValidationChain } from "express-validator";
+
+export default new (class {
+  createOrder(): ValidationChain[] {
+    return [
+      check("firstName")
+        .notEmpty()
+        .withMessage("'firstName' field can not be empty.")
+        .isString()
+        .withMessage("'firstName' field must be string."),
+      check("lastName")
+        .notEmpty()
+        .withMessage("'lastName' field can not be empty")
+        .isString()
+        .withMessage("'lastName' field must be string."),
+      check("phone")
+        .notEmpty()
+        .withMessage("'phone' field can not be empty")
+        .isLength({ min: 11, max: 11 })
+        .withMessage("'phone' field is invalid."),
+
+      check("email").isEmail().withMessage("'email' field is invalid."),
+      check("password")
+        .isLength({ min: 8 })
+        .withMessage(
+          "The password must be more than 8 character and contain alphatic character."
+        )
+        .custom((value: string) => {
+          const lettersCount: number = value
+            .split("")
+            .filter((char: string) => char.match(/[a-zA-Z]/)).length;
+          if (lettersCount >= 2) {
+            return true;
+          }
+          throw "The password must contain at lest 2 alphatic character.";
+        }),
+    ];
+  }
+  private validatePassword(value: string): boolean {
+    const lettersCount: number = value
+      .split("")
+      .filter((char: string) => char.match(/[a-zA-Z]/)).length;
+    if (lettersCount >= 2) {
+      return true;
+    }
+    throw "The password must contain at lest 2 alphatic character.";
+  }
+})();
