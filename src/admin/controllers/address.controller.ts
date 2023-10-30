2a0fd878430cf8db972997ad2fcb664446ee8ad7
Farham-Zaker <farham.zaker007@gmail.com>
2023-10-31 03:03:00 +0330

    feat(admin-address): Add controller for '/get' route

--- src/admin/controllers/address.controller.ts
+++ src/admin/controllers/address.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import prismaService from "../../prisma/prismaService";
+import { AddressTypes } from "../interfaces/address.interface";
 
 export default new (class {
   async createAddress(req: Request, res: Response): Promise<void> {
@@ -30,6 +31,50 @@ export default new (class {
         message: "Error",
         statusCode: 500,
         response: "An error occurred while creating address.",
+      });
+    }
+  }
+  async getAddresses(req: Request, res: Response): Promise<void> {
+    const { searchTerm, user, userId } = req.query;
+    try {
+      const contain: { contains: string } = {
+        contains: searchTerm as string,
+      };
+      let include: any = {};
+      if (user === "true") {
+        include = { user: true };
+      }
+
+      const addresses: AddressTypes[] = await prismaService.addreesses.findMany(
+        {
+          where: {
+            OR: [
+              { addressId: contain },
+              { country: contain },
+              { state: contain },
+              { city: contain },
+              { zone: contain },
+              { apartmentUnite: Number(contain) },
+              { postalCode: contain },
+              { userId: contain },
+            ],
+            userId: userId as string,
+          },
+          include,
+        }
+      );
+
+      res.status(200).json({
+        message: "Success",
+        statusCode: 200,
+        addresses,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "Error",
+        statusCode: 500,
+        response: "An error occurred while getting addresses of users.",
       });
     }
   }
--- src/admin/routes/address.route.ts
+++ src/admin/routes/address.route.ts
@@ -14,6 +14,7 @@ router.get(
   "/get",
   addressValidator.getAddresses(),
   validationResults,
+  addressController.getAddresses
 );
 
 export default router;
