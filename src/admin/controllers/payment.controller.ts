d8739d5205ed441ff1099c6fa12eb5cb3ce6a3ce
Farham-Zaker <farham.zaker007@gmail.com>
2023-11-03 02:12:56 +0330

    refactor(admin-payment): Update error responses

--- src/admin/controllers/payment.controller.ts
+++ src/admin/controllers/payment.controller.ts
@@ -1,12 +1,13 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import prismaService from "../../prisma/prismaService";
 import { PaymentTypes } from "../interfaces/payment.interface";
 
 export default new (class {
   async createPayment(
     req: Request,
-    res: Response
-  ): Promise<Response<any, Record<string, any>>> {
+    res: Response,
+    next: NextFunction
+  ): Promise<Response<any, Record<string, any>> | void> {
     const { authorityId, status, amount, orderId } = req.body;
 
     try {
@@ -36,15 +37,14 @@ export default new (class {
         response: "Desire payment invoice was added successfully.",
       });
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({
-        message: "Success",
-        statusCode: 500,
-        response: "An error occurred while creating payment invoice.",
-      });
+      next(error);
     }
   }
-  async getAllPaynent(req: Request, res: Response): Promise<void> {
+  async getAllPaynent(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     try {
       const payments: PaymentTypes[] = await prismaService.payments.findMany({
         include: {
@@ -57,18 +57,14 @@ export default new (class {
         payments,
       });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        message: "Success",
-        statusCode: 500,
-        response: "An error occurred while getting payment invoice.",
-      });
+      next(error);
     }
   }
   async getPaymentById(
     req: Request,
-    res: Response
-  ): Promise<Response<any, Record<string, any>>> {
+    res: Response,
+    next: NextFunction
+  ): Promise<Response<any, Record<string, any>> | void> {
     const paymentId = req.params.paymentId;
     try {
       const payment: PaymentTypes | null =
@@ -92,15 +88,14 @@ export default new (class {
         payment,
       });
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({
-        message: "Success",
-        statusCode: 500,
-        response: "An error occurred while getting payment invoice.",
-      });
+      next(error);
     }
   }
-  async updatePayment(req: Request, res: Response): Promise<void> {
+  async updatePayment(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     const { paymentId, authorityId, amount, status, orderId } = req.body;
 
     try {
@@ -114,15 +109,14 @@ export default new (class {
         response: "Desire payment invoice was updated successfully,",
       });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        message: "Success",
-        statusCode: 500,
-        response: "An error occurred while updating payment invoice.",
-      });
+      next(error);
     }
   }
-  async deletePayment(req: Request, res: Response): Promise<void> {
+  async deletePayment(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     const paymentId = req.params.paymentId;
     try {
       await prismaService.payments.delete({
@@ -136,12 +130,7 @@ export default new (class {
         response: "Desire payment invoice was deleted successfully.",
       });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({
-        message: "Success",
-        statusCode: 500,
-        response: "An error occurred while deleting payment invoice.",
-      });
+      next(error);
     }
   }
 })();
