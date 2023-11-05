2931c34a2fc4d95ad16185f0eb408180bfc5f45f
Farham-Zaker <farham.zaker007@gmail.com>
2023-11-05 10:42:54 +0330

    feat(swagger): Add auth-related paths and definitions

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -173,5 +173,109 @@ export default {
         },
       },
     },
+    "/auth/register": {
+      post: {
+        tags: ["Authentication"],
+        summary: "Register a new user",
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  firstName: {
+                    type: "string",
+                    description: "User's first name",
+                  },
+                  lastName: {
+                    type: "string",
+                    description: "User's last name",
+                  },
+                  email: {
+                    type: "string",
+                    description: "User's email",
+                  },
+                  phone: {
+                    type: "string",
+                    description: "User's phone number",
+                  },
+                  password: {
+                    type: "string",
+                    description: "User's password",
+                  },
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          201: {
+            description: "User registered successfully",
+          },
+          409: {
+            description:
+              "Conflict - User with similar information already exists",
+          },
+          500: {
+            description: "Internal Server Error",
+          },
+        },
+      },
+    },
+    "/auth/login": {
+      post: {
+        tags: ["Authentication"],
+
+        summary: "Login as a user",
+        requestBody: {
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  phoneOrEmail: {
+                    type: "string",
+                    description: "User's phone number or email",
+                  },
+                  password: {
+                    type: "string",
+                    description: "User's password",
+                  },
+                },
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description: "User logged in successfully",
+          },
+          401: {
+            description: "Unauthorized - Incorrect password",
+          },
+          404: {
+            description: "Not Found - User with given profile not found",
+          },
+          500: {
+            description: "Internal Server Error",
+          },
+        },
+      },
+    },
+    "/google": {
+      get: {
+        tags: ["Authentication"],
+        summary: "Google OAuth login",
+        operationId: "googleOAuthLogin",
+        responses: {
+          "200": {
+            description: "OK - Google OAuth login successful",
+          },
+          "500": {
+            description: "Internal Server Error",
+          },
+        },
+      },
+    },
   },
 };
