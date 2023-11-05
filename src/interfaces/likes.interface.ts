5730927220a25f144849c813ea51f775aaf58158
Farham-Zaker <farham.zaker007@gmail.com>
2023-11-05 13:38:18 +0330

    refactor(likes): Updae interface

--- src/interfaces/likes.interface.ts
+++ src/interfaces/likes.interface.ts
@@ -1,31 +1,24 @@
 export interface ProductsInLikesTypes {
-  id: string;
-  product:
-    | {
-        productId: string;
-        title: string;
-        price: number;
-        images?: {
-          imageId: string;
-          imageUrl: string;
-          productId: string;
-          createdAt: Date;
-        }[];
-        discountStatus: number | null;
-        discountPercent: number | null;
-        dicountEndTime: Date | null;
-        discountedPrice: number | null;
-        category: {
-          name: string;
-        };
-        inventories: {
-          colors: {
-            colorId: string;
-            name: string;
-            hexCode: string;
-          };
-          quantity: number;
-        };
-      }[]
-    | [];
+  likeId: string;
+  product: ProductTypes;
+}
+interface ProductTypes {
+  productId: string;
+  title: string;
+  price: number;
+  finalPrice: number;
+  images: imageTypes[];
+  inventories: InventoryTypes[];
+}
+interface imageTypes {
+  imageId: string;
+  imageUrl: string;
+  productId: string;
+  createdAt: Date;
+}
+interface InventoryTypes {
+  inventoryId: string;
+  quantity: number;
+  productId: string;
+  colorId: string;
 }
