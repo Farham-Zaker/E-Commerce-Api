76ebcdeb2a4dd3e3c5027c39f0f0810780d9e8db
Farham-Zaker <farham.zaker007@gmail.com>
2023-11-06 10:06:35 +0330

    refactor(admin-sub route): Update all sub routes

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -3,37 +3,37 @@ const router = Router();
 import isAdmin from "../middlewares/isAdmin";
 import productRoute from "./routes/product.route";
 
-router.use("/product", isAdmin, productRoute);
+router.use("/products", isAdmin, productRoute);
 
 import categoryRoute from "./routes/category.route";
-router.use("/category", isAdmin, categoryRoute);
+router.use("/categories", isAdmin, categoryRoute);
 
 import colorRoute from "./routes/color.route";
-router.use("/color", isAdmin, colorRoute);
+router.use("/colors", isAdmin, colorRoute);
 
 import inventoryRoute from "./routes/inventory.route";
-router.use("/inventory", isAdmin, inventoryRoute);
+router.use("/inventories", isAdmin, inventoryRoute);
 
 import orderRoute from "./routes/order.route";
-router.use("/order", isAdmin, orderRoute);
+router.use("/orders", isAdmin, orderRoute);
 
 import orderItemsRoute from "./routes/orderItems.route";
 router.use("/orderItems", isAdmin, orderItemsRoute);
 
 import paymentRoute from "./routes/payment.route";
-router.use("/payment", isAdmin, paymentRoute);
+router.use("/payments", isAdmin, paymentRoute);
 
 import cartRoute from "./routes/cart.route";
-router.use("/cart", isAdmin, cartRoute);
+router.use("/carts", isAdmin, cartRoute);
 
 import userRoute from "./routes/user.route";
-router.use("/user", isAdmin, userRoute);
+router.use("/users", isAdmin, userRoute);
 
 import addressRoute from "./routes/address.route";
-router.use("/address", isAdmin, addressRoute);
+router.use("/addresses", isAdmin, addressRoute);
 
 import commentRoute from "./routes/comment.route";
-router.use("/comment", isAdmin, commentRoute);
+router.use("/comments", isAdmin, commentRoute);
 
 import likeRoute from "./routes/likes.route";
 router.use("/likes", isAdmin, likeRoute);
